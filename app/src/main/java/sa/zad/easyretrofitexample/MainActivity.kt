312b397a693d16f641f9e498b103f808c7794559
saizad <mrprasla@gmail.com>
2024-02-21 11:15:40 +0530

    Version Updated to 34

--- app/src/main/java/sa/zad/easyretrofitexample/MainActivity.kt
+++ app/src/main/java/sa/zad/easyretrofitexample/MainActivity.kt
@@ -1,29 +1,8 @@
 package sa.zad.easyretrofitexample
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import sa.zad.easyretrofitexample.BaseActivity.getActivityIntent
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        response_post.setOnClickListener {
-            startActivity(getActivityIntent(NeverErrorObservableActivity::class.java, this))
-        }
 
-        observable_get.setOnClickListener {
-            startActivity(getActivityIntent(ResultObservableActivity::class.java, this))
-        }
-
-        download.setOnClickListener {
-            startActivity(getActivityIntent(DownloadObservableActivity::class.java, this))
-        }
-
-        upload.setOnClickListener {
-            startActivity(getActivityIntent(UploadObservableActivity::class.java, this))
-        }
-    }
 }
